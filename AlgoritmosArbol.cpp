424d3699a9cac3b5a272a60d30590fa9b01f0425
Akkarim <luisda7cr@gmail.com>
2017-10-18 18:52:35 -0600

    Cambio
    Prb de app

--- AlgoritmosArbol.cpp
+++ AlgoritmosArbol.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   AlgoritmosArbol.cpp
  * Author: Jimmy
@@ -43,6 +37,7 @@ nodo AlgoritmosArbol::hermanoIzq(Arbol* a, nodo n) {
 }
 
 bool AlgoritmosArbol::etiquetasRepetidas(Arbol* A) {
+    
 }
 
 int AlgoritmosArbol::numNivelesPorNiveles(Arbol* A, nodo n) {
